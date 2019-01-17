a80036cc5244147829e95b1663b039d9ebbcac03
Daniel Chain <DanielChain3@gmail.com>
2019-01-17 14:31:53 -0500

    update files

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from os import listdir
 from os.path import isfile, join
+from pymdeco import services
 import piexif
+import sys
 
 DATE_VALUE = 36867      #described by the spec
+srv = services.FileMetadataService()
 
-def main(
+def main():
+    dirs = sys.argv[1:]
+    for directory in dirs:
+        os.chdir(directory)
+        files = [f for f in listdir(mypath) if isfile(join(mypath, f))]
+        for fileName in files:
+            if fileName.lower().endswith((".jpg", ".jpeg", ".heic")):
+                os.rename(fileName, getPhotoDate(fileName))
 
-def getDate(fileName):
+
+def getPhotoDate(fileName):
     exif_dict = piexif.load(fileName)
     exif = exif_dict["Exif"]        #Get the exif dict
     date = exif[DATE_VALUE]
     return date
 
-exif_dict = piexif.load("1.jpg")["Exif"]
+def getVideoDate(fileName):
+    meta = srv.get_metadata(fileName).to_json(indent=2))
+    data = meta["file_timestamps"]["modified"]
 
+if __name__ == "__main__":
+    main()
